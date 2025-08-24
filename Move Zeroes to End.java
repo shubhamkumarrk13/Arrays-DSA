aa9ef3c57e7c04b7e32199e2ee7c33a9dbcaa436
shubhamkumarrk13 <13ishubhamkumar@gmail.com>
2025-08-24 20:45:31 +0530

    Create Move Zeroes to End.java

---
+++ Move Zeroes to End.java
@@ -0,0 +1,28 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        int left = 0;
+
+        int right = 0;
+
+        while(right < nums.length) {
+
+            if(nums[right] != 0) {
+
+                nums[left] = nums[right];
+
+                left++;
+            }
+
+            right++;
+        }
+
+        while(left < nums.length) {
+
+            nums[left] = 0;
+
+            left++;
+        }
+        
+    }
+}
