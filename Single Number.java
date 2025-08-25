c137e1498d5772d81edfa4481597c9c86899e431
shubhamkumarrk13 <13ishubhamkumar@gmail.com>
2025-08-25 20:41:58 +0530

    Create Single Number.java

---
+++ Single Number.java
@@ -0,0 +1,16 @@
+// Problem Link : https://leetcode.com/problems/single-number/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int xor = 0;
+
+        for(int i=0 ; i<nums.length ; i++) {
+
+            xor = xor ^ nums[i];
+        }
+
+        return xor;
+        
+    }
+}
