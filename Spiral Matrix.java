1ed58f096d64ac2d68067bfd52f857e5caac7ea1
shubhamkumarrk13 <13ishubhamkumar@gmail.com>
2025-08-28 10:38:59 +0530

    Create Spiral Matrix.java

---
+++ Spiral Matrix.java
@@ -0,0 +1,58 @@
+// Problem Link : https://leetcode.com/problems/spiral-matrix/
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        List<Integer> result = new ArrayList<>();
+
+        int left = 0;
+
+        int right = matrix[0].length - 1;
+
+        int top = 0;
+
+        int bottom = matrix.length - 1;
+
+        while(top <= bottom && left <= right) {
+
+            if(top <= bottom) {
+
+                for(int i=left ; i<=right ; i++) {
+
+                    result.add(matrix[top][i]);
+                }
+                top++;
+            }
+
+            if(left <= right) {
+
+                for(int i=top ; i<=bottom ; i++) {
+
+                    result.add(matrix[i][right]);
+                }
+                right--;
+            }
+
+            if(top <= bottom) {
+
+                for(int i=right ; i>=left ; i--) {
+
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if(left <= right) {
+
+                for(int i=bottom ; i>=top ; i--) {
+
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+
+        return result;
+        
+    }
+}
